5ce4d40b0539f2c536fc8122fc8a18eb310690dd
MSR333UA <matviivserhii@gmail.com>
2023-05-05 09:25:46 +0300

    added styles to ListScreen, DetailsScreen

--- src/pages/DetailsScreen/DetailsScreen.styled.js
+++ src/pages/DetailsScreen/DetailsScreen.styled.js
@@ -4,37 +4,116 @@ import { Link, NavLink } from 'react-router-dom';
 export const MovieDetailsStyled = styled.div``;
 
 export const AdditionalStyled = styled.ul`
-
+  padding: ${p => p.theme.space[2]}px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: ${p => p.theme.space[4]}px;
 `;
 
 export const LinkStyled = styled(Link)`
+  display: block;
 
+  width: ${p => p.theme.space[7]}px;
+
+  padding: ${p => p.theme.space[3]}px;
+  margin-bottom: 10px;
+  background-color: ${p => p.theme.colors.red};
+  color: ${p => p.theme.colors.white};
+  text-align: center;
+  font-size: 16px;
+
+  border-radius: 8px;
+  font-family: 'Roboto-Bold';
+  :hover:not(.active) {
+    color: ${p => p.theme.colors.red};
+    background-color: ${p => p.theme.colors.white};
+    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+      0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 250px;
+    font-size: 14px;
+    line-height: 18px;
+    margin: auto;
+  }
 `;
 export const Wrap = styled.div`
- 
+  display: flex;
+  gap: 30px;
 `;
 export const ImgWrap = styled.div`
+  display: flex;
+  justify-content: start;
+  gap: ${p => p.theme.space[4]}px;
 
+  padding: ${p => p.theme.space[2]}px;
+  img {
+    height: 30vw;
+  }
+  @media screen and (max-width: 768px) {
+    margin: auto;
+  }
 `;
 
 export const TextWrap = styled.div``;
 
 export const Title = styled.h1`
+  display: block;
 
+  font-size: 24px;
+
+  font-style: normal;
+  font-weight: 700;
+
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
 `;
 
 export const TitleSecond = styled.p`
+  display: block;
 
+  margin: ${p => p.theme.space[2]}px;
+
+  font-size: 18px;
+
+  font-style: italic;
+  font-weight: 700;
+
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
 `;
 
 export const ListItem = styled.li`
+  margin-bottom: ${p => p.theme.space[4]}px;
+  width: 200px;
 
+  overflow: hidden;
+
+  font-size: 12px;
 `;
 
 export const SummaryText = styled.p`
-
+  margin-top: 20px;
 `;
 
 export const NavLinkStyled = styled(NavLink)`
+  display: block;
 
+  font-size: 12px;
+  line-height: 24px;
+  font-family: 'Roboto-Regular';
+  margin-top: 10px;
+  text-decoration: none;
+  text-align: center;
+  color: ${p => p.theme.colors.text};
+  border-radius: 8px;
+  background-color: ${p => p.theme.colors.lightred};
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+
+  color: ${p => p.theme.colors.black};
+
+  :hover:not(.active) {
+    color: ${p => p.theme.colors.red};
+  }
 `;
--- src/pages/ListScreen/ListScreen.styled.js
+++ src/pages/ListScreen/ListScreen.styled.js
@@ -12,18 +12,89 @@ export const ErrorMessage = styled.div`
   text-align: center;
   margin: ${p => p.theme.space[4]}px;
 `;
+export const Container = styled.div`
+  width: 100vw;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: auto;
+  margin-right: auto;
 
-export const Container = styled.div``;
+  @media screen and (min-width: 320px) {
+    width: 320px;
+  }
 
-export const MoviesListStyled = styled.ul``;
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
 
-export const ListItem = styled.li``;
-export const WrapperText = styled.div``;
+  @media screen and (min-width: 1440px) {
+    width: 1440px;
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+`;
 
-export const ScoreText = styled.p``;
+export const MoviesListStyled = styled.ul`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  /* flex-direction: row; */
 
-export const Title = styled.h1``;
+  justify-content: center;
+  margin-top: 100px;
+  gap: ${p => p.theme.space[5]}px;
+  row-gap: ${p => p.theme.space[6]}px;
+`;
 
-export const TitleMovie = styled.h2``;
+export const ListItem = styled.li`
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  border-radius: 8px;
 
-export const Img = styled.img``;
+  :hover,
+  :focus {
+    transform: scale(1.01);
+    transition: transform 150ms;
+  }
+  width: 268px;
+  background-color: #21212152;
+`;
+export const WrapperText = styled.div`
+  padding: 10px;
+  text-align: center;
+  letter-spacing: 0.06em;
+`;
+
+export const ScoreText = styled.p`
+  font-family: 'Roboto-Medium';
+  font-size: 12px;
+  padding-top: 10px;
+
+  color: ${p => p.theme.colors.text};
+`;
+
+export const Title = styled.h1`
+  padding: ${p => p.theme.space[4]}px;
+
+  font-family: 'Roboto-Bold';
+
+  text-transform: uppercase;
+
+  color: ${p => p.theme.colors.text};
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+`;
+
+export const TitleMovie = styled.h2`
+  font-size: 16px;
+  text-align: center;
+  font-family: 'Roboto-Bold';
+  color: #711313;
+`;
+
+export const Img = styled.img`
+  display: block;
+  width: 100%;
+  height: 80%;
+  margin: 0;
+  border-radius: 8px 8px 0 0;
+`;
