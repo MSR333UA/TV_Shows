184163dfb8841dfbbbd8f120b35aceb7426c4f87
MSR333UA <matviivserhii@gmail.com>
2023-05-05 12:50:06 +0300

    fixed spinner

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,18 @@
 import { ColorRing } from 'react-loader-spinner';
+import { Wrap } from './Loader.styled';
 
 export const Loader = () => {
-
-return (
-<ColorRing
-  visible={true}
-  height="80"
-  width="80"
-  ariaLabel="blocks-loading"
-  wrapperStyle={{}}
-  wrapperClass="blocks-wrapper"
-  colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-/>
-)}
+  return (
+    <Wrap>
+      <ColorRing
+        visible={true}
+        height="150"
+        width="150"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+    </Wrap>
+  );
+};
